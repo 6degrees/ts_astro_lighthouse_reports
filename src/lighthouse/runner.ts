1c3c08b28e05a96f72dfa679b88b45e2f57ef8de
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2024-06-23 23:37:35 +0300

    initial version

---
+++ src/lighthouse/runner.ts
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import lighthouse, { type Flags, type RunnerResult } from 'lighthouse';
+import * as chromeLauncher from 'chrome-launcher';
+import dayjs from 'dayjs';
+import path from 'path';
+import pLimit from 'p-limit';
+
+import {websites} from './websites.json';
+
+const __dirname = './public/';
+const limit = pLimit(1); // Limit concurrency to 4
+
+const runLighthouse = async (url: string) => {
+    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+
+    const options: Flags = {
+        logLevel: 'info',
+        output: ['html', 'json'],
+        port: chrome.port,
+        disableFullPageScreenshot: true,
+        formFactor: 'desktop',
+        screenEmulation: {
+            disabled: true,
+        },
+    };
+
+    const runnerResult: RunnerResult = await lighthouse(url, options);
+
+    const date = dayjs().format('YYYY-MM-DD');
+    const siteName = new URL(url).hostname;
+    const reportPath = path.join(__dirname, 'reports', siteName, date);
+    const livePath = path.join(__dirname, 'reports', siteName, 'live');
+
+    if (!fs.existsSync(reportPath)) {
+        fs.mkdirSync(reportPath, { recursive: true });
+    }
+
+    if (!fs.existsSync(livePath)) {
+        fs.mkdirSync(livePath, { recursive: true });
+    }
+
+    // Write HTML report
+    const reportHtml = runnerResult.report[0];
+    fs.writeFileSync(path.join(reportPath, `report.html`), reportHtml);
+    fs.writeFileSync(path.join(livePath, `report.html`), reportHtml);
+
+    // Write JSON report
+    const reportJson = runnerResult.report[1];
+    fs.writeFileSync(path.join(reportPath, `report.json`), reportJson);
+    fs.writeFileSync(path.join(livePath, `report.json`), reportJson);
+
+    console.log(`Report is done for ${runnerResult.lhr.finalDisplayedUrl}`);
+    console.log(`Performance score was ${runnerResult.lhr.categories.performance.score * 100}`);
+
+    await chrome.kill();
+};
+
+const runAllLighthouse = async () => {
+    const promises = websites.map((site) => limit(() => runLighthouse(site.url)));
+    await Promise.all(promises);
+};
+
+runAllLighthouse().catch((error) => {
+    console.error('Error running Lighthouse:', error);
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+
+	plugins: [
+		require('flowbite/plugin')({
+			charts: true,
+		}),
+	  
+	],
+}
